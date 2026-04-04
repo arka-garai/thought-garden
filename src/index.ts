05456136afd45a740ab6572fb7e10a108844021f
arka <arkagarai292@gmail.com>
2026-04-04 08:57:36 +0530

    feat: update index.ts with route imports and refactor to use new folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,18 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
+import authRoutes from "./routes/auth-routes.js";
+import contentRoutes from "./routes/content-routes.js";
+import brainRoutes from "./routes/brain-routes.js";
 
 const app = express();
 app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) => {
+app.use("/api/v1", authRoutes);
+app.use("/api/v1/content", contentRoutes);
+app.use("/api/v1/brain", brainRoutes);
 
-})
-app.post("/api/v1/signin", (req, res) => {
-
-})
-app.post("/api/v1/content", (req, res) => {
-
-})
-app.get("/api/v1/content", (req, res) => {
-
-})
-app.delete("/api/v1/content", (req, res) => {
-
-})
-app.post("/api/v1/brain/share", (req, res) => {
-
-})
-app.post("/api/v1/brain/:shareLink", (req, res) => {
-
-})
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3345;
 
 const start = async () => {
     await mongoose.connect(process.env.MONGO_URI!);
@@ -40,5 +24,3 @@ const start = async () => {
 };
 
 start();
-
-
